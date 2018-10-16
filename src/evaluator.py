0d9df1d73010c4caecb34aee65edd40dc92b8130
ZuxiuWang <e0267728@u.nus.edu>
2018-10-16 10:23:03 +0800

    calculator fitness

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,8 +1,7 @@
-import src.fuzzy as fuzzy
-import src.generator as generator
+import fuzzy
+import generator
 
 eva_data = None
-
 
 class Evaluator:
 
@@ -13,6 +12,29 @@ class Evaluator:
     @property
     def generator(self):
         return self._generator
+
+    def CalcBrokerage(self,volume, price):
+        BrokerageRate = 0.2
+        minFee = 30
+        fee = volume * price * BrokerageRate / 100
+        if fee < minFee:
+            return 30
+        else:
+            return fee
+
+    def trade(self,data, Hold, Money):
+        if data['Signal'] > 0:  # signal is buy, money must be enough to buy, otherwise can not buy
+            if Money > 0:
+                buy = round(Money * data['Signal'] / data['High '])
+                Hold = Hold + buy
+                Money = Money - buy * data['High '] - self.CalcBrokerage(buy, data['High '])
+        if data['Signal'] < 0:  # signal is sell, hold must be enough to sell, otherwise can not sell
+            if Hold > 0:  # data['Signal']<0
+                sell = -Hold * data['Signal']
+                Hold = Hold - sell
+                Money = Money + sell * data['Low'] - self.CalcBrokerage(sell, data['Low'])
+        fortune = Hold * data['Close'] + Money
+        return Hold, Money, fortune
 
     def evaluate(self, ind):
         """
@@ -26,9 +48,18 @@ class Evaluator:
 
         # Calculate the signals according to the fuzzy rule set
         decision = fuzzy.DecisionMaker(rule_set, self._data)
-        signals = decision.defuzzify(self._data)
-        # TODO: need to define the data format of the signal. E.g. DataFrame with DateTimeIndex
 
+        #signals = pd.DataFrame([0.5, -0.4, 0.2, 0.4, 0.1, -0.2, -0.3, 0.2, 0.3, 0.1], index=self._data.index,columns=['Signal'])
+        signals = decision.defuzzify(self._data)  #signal is a dataframe
+
+        self._data['Signal']=signals
         # Calculate the fitness value according to the trading signals
+        Hold=0
+        Money=10000000
+        Fortune = []
+        for i, row in self._data.iterrows():
+            Hold, Money, fortune = self.trade(row, Hold, Money)
+            Fortune.append(fortune)
+        self._data['Fortune'] = Fortune
 
-        return 0
+        return self._data.iloc[-1,9]
